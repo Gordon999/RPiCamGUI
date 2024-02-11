98644200149cad69d617dc5ddf76320cfefb79bc
Gordon999 <Gordon999@users.noreply.github.com>
2024-02-11 09:21:11 +0000

    v4.83 for Bullseye Legacy

--- LibCamGUI.py
+++ LibCamGUI.py
@@ -32,7 +32,7 @@ import math
 from gpiozero import Button
 import random
 
-version = 4.83L
+version = 4.83
 
 # Set displayed preview image size (must be less than screen size to allow for the menu!!)
 # Recommended 640x480 (Pi 7" or other 800x480 screen), 720x540 (FOR SQUARE HYPERPIXEL DISPLAY),
